8e6c0cff92bcb04f3e0e6fec1ad37ec7554ed5f7
PERUSOMULA SRINIVASA CHARI <127323306+chari6268@users.noreply.github.com>
2025-06-12 04:57:33 +0000

    add dotenv configuration and mongoose connection to app.js; update .gitignore to include .env

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on("error",(e) => console.error(e))
+db.once("open",() => console.log('Database connecte..!'))
 
 app.listen(3000,() => console.log("Server started"))
